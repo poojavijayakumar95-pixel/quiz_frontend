bf89dae5d09cfc5fa1b627ed6a570849f8196920
Pooja <poojavijayakumar95@gmail.com>
2026-04-06 14:25:45 +0530

    Modified code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: BASE_URL,
 });
 
 // Automatically attach the JWT token if it exists
